c3fcd5548a05a1efe74690c9d94be4eb9b219564
RabbitMask <37649548+rabbitmask@users.noreply.github.com>
2020-05-25 20:20:14 +0800

    Update console.py

--- Core/console.py
+++ Core/console.py
@@ -8,39 +8,51 @@
 |_| \_\__,_|_.__/|_.__/|_|\__|_|  |_|\__,_|___/_|\_\
 '''
 import argparse
-from Modules import domain_ip, crt_domain, asn_cidr, censys_ip, ip_whois, domain_whois
+from Modules_Active import hawkeye
+from Modules_Passive import domain_ip, crt_domain, asn_cidr, censys_ip, ip_whois, domain_whois
 from Tools import cidr_ip, kill_repeat, email_dig
 from Cracks import mysql_crack, redis_crack, ftp_crack, ssh_crack
 
-aboutdic='\tlevel 1 password 10\t\nlevel 2 password 100\t\nlevel 3 password 1000\t\nlevel 4 password 10000\t\nlevel 5 password 100000\t\nlevel 0 From yourself! :)'
+aboutdic='  level 1 password 10\n  level 2 password 100\n  level 3 password 1000\n  level 4 password 10000\n  level 5 password 100000\n  level 0 From yourself! :)'
 
 
 def Console():
     parser = argparse.ArgumentParser()
-    ahf_modules = parser.add_argument_group('AHF Modules')
+    ahf_modules_passive = parser.add_argument_group('AHF Modules_Passive')
+    ahf_modules_active = parser.add_argument_group('AHF Modules_Active')
     ahf_tools = parser.add_argument_group('AHF Tools')
     ahf_cracks = parser.add_argument_group('AHF Cracks')
 
+
+########################################################################################################################
+
+
+    #被动式扫描模块
     parser.add_argument("-t", dest='target', help="target")
     parser.add_argument("-p", dest='port',help="port")
     parser.add_argument("-u", dest='user', help="user")
     parser.add_argument("-l", dest='level', help="level")
 
+    #主动式扫描模块
+    ahf_modules_active.add_argument("-hawkeye", dest='hawkeye',help="Cidr Site 侦测")
+
+    #被动式扫描模块
+    ahf_modules_passive.add_argument("-asn", dest='asn',help="ASN查询ICDR")
+    ahf_modules_passive.add_argument("-censys", dest='censys',help="CENSYS API查询")
+    ahf_modules_passive.add_argument("-crt", dest='crt',help="证书透明度查询域名")
+    ahf_modules_passive.add_argument("-dns", dest='dns',help="DNS A记录解析")
+    ahf_modules_passive.add_argument("-fofa", dest='fofa',help="FOFA API查询(待开放)")
+    ahf_modules_passive.add_argument("-ipwhois", dest='ipwhois',help="IP Whois查询")
+    ahf_modules_passive.add_argument("-whois", dest='whois',help="域名Whois查询")
+    ahf_modules_passive.add_argument("-shadon", dest='shadon',help="Shadon API查询(待开放)")
 
 
-    ahf_modules.add_argument("-asn", dest='asn',help="ASN查询ICDR")
-    ahf_modules.add_argument("-censys", dest='censys',help="CENSYS API查询")
-    ahf_modules.add_argument("-crt", dest='crt',help="证书透明度查询域名")
-    ahf_modules.add_argument("-dns", dest='dns',help="DNS A记录解析")
-    ahf_modules.add_argument("-fofa", dest='fofa',help="FOFA API查询(待开放)")
-    ahf_modules.add_argument("-ipwhois", dest='ipwhois',help="IP Whois查询")
-    ahf_modules.add_argument("-whois", dest='whois',help="域名Whois查询")
-    ahf_modules.add_argument("-shadon", dest='shadon',help="Shadon API查询(待开放)")
-
+    #资产收集工具模块
     ahf_tools.add_argument("-cidr", dest='cidr',help="Cidr转换为IP范围")
     ahf_tools.add_argument("-emaildig", dest='emaildig',help="Email挖掘工具(入口:文件)")
     ahf_tools.add_argument("-removal", dest='removal',help="数据去重工具(入口:文件)")
 
+    #轻量级爆破模块
     ahf_cracks.add_argument("-mysqlc", dest='mysqlc',default='',help="MySQL爆破")
     ahf_cracks.add_argument("-redisc", dest='redisc', default='', help="redis爆破")
     ahf_cracks.add_argument("-ftpc", dest='ftpc', default='', help="FTP爆破")
@@ -48,7 +60,16 @@ def Console():
 
     args = parser.parse_args()
 
-    if args.asn:
+
+########################################################################################################################
+
+
+    #主动式扫描模块
+    if args.hawkeye:
+        hawkeye.run(args.hawkeye)
+
+    #被动式扫描模块
+    elif args.asn:
         asn_cidr.run(args.asn)
     elif args.crt:
         crt_domain.run(args.crt)
@@ -60,12 +81,16 @@ def Console():
         ip_whois.run(args.ipwhois)
     elif args.whois:
         domain_whois.run(args.whois)
+
+    #资产收集工具模块
     elif args.cidr:
         cidr_ip.run(args.cidr)
     elif args.emaildig:
         email_dig.run(args.emaildig)
     elif args.removal:
         kill_repeat.run(args.removal)
+
+    #轻量级爆破模块
     elif args.mysqlc:
         try:
             mysql_crack.run(args.mysqlc,args.port,args.user,args.level)
@@ -86,3 +111,6 @@ def Console():
             ssh_crack.run(args.sshc,args.port,args.user,args.level)
         except:
             print('eg: python AssetsHunter.py -sshc 127.0.0.1 -p 22 -u rabbit -l 1\n'+aboutdic)
+
+
+########################################################################################################################
