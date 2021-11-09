e6369082401e0532af4b64381f116578655c009b
rabbitmask <rabbitmask@163.com>
2021-11-09 16:02:08 +0800

    V 1.2 更新

--- Core/console.py
+++ Core/console.py
@@ -11,9 +11,6 @@ import argparse
 from Modules_Active import hawkeye, inforisk, whatcms
 from Modules_Passive import domain_ip, crt_domain, asn_cidr, censys_ip, ip_whois, domain_whois
 from Tools import cidr_ip, kill_repeat, email_dig
-from Cracks import mysql_crack, redis_crack, ftp_crack, ssh_crack
-
-aboutdic='  level 1 password 10\n  level 2 password 100\n  level 3 password 1000\n  level 4 password 10000\n  level 5 password 100000\n  level 0 From yourself! :)'
 
 
 def Console():
@@ -21,17 +18,9 @@ def Console():
     ahf_modules_passive = parser.add_argument_group('AHF Modules_Passive')
     ahf_modules_active = parser.add_argument_group('AHF Modules_Active')
     ahf_tools = parser.add_argument_group('AHF Tools')
-    ahf_cracks = parser.add_argument_group('AHF Cracks')
 
 
 ########################################################################################################################
-
-
-    #被动式扫描模块
-    parser.add_argument("-t", dest='target', help="target")
-    parser.add_argument("-p", dest='port',help="port")
-    parser.add_argument("-u", dest='user', help="user")
-    parser.add_argument("-l", dest='level', help="level")
 
     #主动式扫描模块
     ahf_modules_active.add_argument("-hawkeye", dest='hawkeye',help="Cidr Site 侦测")
@@ -53,12 +42,6 @@ def Console():
     ahf_tools.add_argument("-cidr", dest='cidr',help="Cidr转换为IP范围")
     ahf_tools.add_argument("-emaildig", dest='emaildig',help="Email挖掘工具(入口:文件)")
     ahf_tools.add_argument("-removal", dest='removal',help="数据去重工具(入口:文件)")
-
-    #轻量级爆破模块
-    ahf_cracks.add_argument("-mysqlc", dest='mysqlc',default='',help="MySQL爆破")
-    ahf_cracks.add_argument("-redisc", dest='redisc', default='', help="redis爆破")
-    ahf_cracks.add_argument("-ftpc", dest='ftpc', default='', help="FTP爆破")
-    ahf_cracks.add_argument("-sshc", dest='sshc', default='', help="SSH爆破")
 
     args = parser.parse_args()
 
@@ -95,28 +78,6 @@ def Console():
         email_dig.run(args.emaildig)
     elif args.removal:
         kill_repeat.run(args.removal)
-
-    #轻量级爆破模块
-    elif args.mysqlc:
-        try:
-            mysql_crack.run(args.mysqlc,args.port,args.user,args.level)
-        except:
-            print('eg: python AssetsHunter.py -mysqlc 127.0.0.1 -p 3306 -u root -l 1\n'+aboutdic)
-    elif args.redisc:
-        try:
-            redis_crack.run(args.redisc,args.port,args.level)
-        except:
-            print('eg: python AssetsHunter.py -redisc 127.0.0.1 -p 6379 -l 1\n'+aboutdic)
-    elif args.ftpc:
-        try:
-            ftp_crack.run(args.ftpc,args.port,args.user,args.level)
-        except:
-            print('eg: python AssetsHunter.py -ftpc 127.0.0.1 -p 21 -u rabbit -l 1\n'+aboutdic)
-    elif args.sshc:
-        try:
-            ssh_crack.run(args.sshc,args.port,args.user,args.level)
-        except:
-            print('eg: python AssetsHunter.py -sshc 127.0.0.1 -p 22 -u rabbit -l 1\n'+aboutdic)
 
 
 ########################################################################################################################
--- Modules_Active/hawkeye.py
+++ Modules_Active/hawkeye.py
@@ -19,7 +19,11 @@ from Tools.cidr_ip import Cidr_ips
 requests.packages.urllib3.disable_warnings()
 
 
-
+def urlcheck(url):
+    if 'http' in url:
+        return url
+    else:
+        return ('http://'+str(url))
 
 def Get_urls(cidr):
     if re.match(r"^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/([1-9]|[1-2]\d|3[0-2])$",cidr):
@@ -53,7 +57,7 @@ def Get_tile(url,res,q):
     q.put(url)
 
 @Save_info
-def Hawkeye(cidr):
+def Hawkeye_cidr(cidr):
     res = Manager().list([])
     p = Pool(30)
     q = Manager().Queue()
@@ -68,9 +72,36 @@ def Hawkeye(cidr):
     else:
         print("CIDR格式输入有误，锤你昂w(ﾟДﾟ)w")
 
+@Save_info
+def Hawkeye_file(filename):
+    res = Manager().list([])
+    p = Pool(30)
+    q = Manager().Queue()
+    try:
+        f=open(filename,'r')
+        urls=f.readlines()
+        f.close()
 
-def run(cidr):
-    Hawkeye(cidr)
+        print('侦测开始~加载任务量：{}条'.format(len(urls)))
+        if urls:
+            for i in urls:
+                p.apply_async(Get_tile, args=(urlcheck(i.replace('\n','')),res,q))
+            p.close()
+            p.join()
+            return res
+        else:
+            print("文件内容错误，锤你昂w(ﾟДﾟ)w")
+    except:
+        print("给我个文件吖喂 w(ﾟДﾟ)w")
+
+
+def run(*args):
+    if len(args)==2:
+        if isinstance(args[1],str):
+            Hawkeye_cidr(args[1])
+        else:
+            Hawkeye_file(args[1])
+
 
 if __name__ == '__main__':
     print(Get_tile('https://github.com/rabbitmask/AssetsHunter'))
